89e285849c669bfbf97aa476454db643cc3e165b
Chikki24 <chikki.mowli@gmail.com>
2023-03-20 12:16:10 +0530

    rel-operators1

---
+++ src/relationaloperators/RelOps.java
@@ -0,0 +1,30 @@
+package relationaloperators;
+import java.util.Scanner;
+public class RelOps {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+int b=sc.nextInt();
+int c=sc.nextInt();
+System.out.println(" a = " +a);
+System.out.println(" b = " +b);
+System.out.println(" c = " +c);
+System.out.println("a+b = " +(a+b));
+System.out.println("a-b = " +(a-b));
+System.out.println("a*b = " +(a*b));
+System.out.println("a/b = " +(a/b));
+System.out.println("a%b = " +(a%b));
+System.out.println("a<b = " +(a<b));
+System.out.println("a>b = " +(a>b));
+System.out.println("a==c = " +(a==c));
+System.out.println("a<= = " +(a<=c));
+System.out.println("a>=c = " +(a>=c));
+System.out.println("b!=c = " +(b!=c));
+System.out.println("b == a+c " +(b==a+c));
+
+	}
+
+}
